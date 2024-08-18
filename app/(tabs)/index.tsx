5b76fddfe9c395212b5aab038543e52d61481a33
kaykyls <dev.kayky@gmail.com>
2024-08-18 15:37:15 -0300

    update: create post ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,12 +9,26 @@ export default function TabOneScreen() {
   const user = null;
 
   if (!user) {
-    return <Redirect href="/login" />;
+    // return <Redirect href="/login" />;
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <View style={styles.post}>
+        <View style={styles.picture}>
+          
+        </View>
+        <View style={styles.content}>
+          <View style={styles.name}>
+            <Text>name</Text>
+            <Text>@username</Text>
+          </View>
+          <View style={styles.text}>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium consectetur natus facere quasi temporibus, perspiciatis architecto ratione commodi accusamus laudantium sunt beatae rem ex optio, dolorum magni quas, ea doloremque?</Text>
+          </View>
+          
+        </View>
+      </View>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
@@ -22,18 +36,36 @@ export default function TabOneScreen() {
 }
 
 const styles = StyleSheet.create({
+  text: {
+    width: '100%',
+  },
+  picture: {
+    width: 40,
+    height: 40,
+    borderRadius: 25,
+    backgroundColor: 'gray',
+  },
+  post: {
+    padding: 16,
+    flexDirection: 'row',
+    gap: 16,
+  },
+  content: {
+    gap: 8,
+    alignItems: 'center',
+    width: '100%',
+    flexShrink: 1,
+  },
+  name: {
+    flexDirection: 'row',
+    gap: 8,
+    alignItems: 'center',
+    width: '100%',
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
   },
   separator: {
-    marginVertical: 30,
     height: 1,
-    width: '80%',
   },
 });
