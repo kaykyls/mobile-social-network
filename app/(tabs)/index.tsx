c8884ddd24f6b50026dd72ece3ca9e55c8bf3bfb
kaykyls <dev.kayky@gmail.com>
2024-08-18 14:28:23 -0300

    chore: Remove unused code and add login redirect

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,21 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { Link } from 'expo-router';
+import { Redirect } from 'expo-router';
 
 export default function TabOneScreen() {
+  const user = null;
+
+  if (!user) {
+    return <Redirect href="/login" />;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
   );
 }
