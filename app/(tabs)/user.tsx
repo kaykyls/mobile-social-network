d6c0c259ee7d3a1860feb2aae07641ba98f5e572
kaykyls <dev.kayky@gmail.com>
2024-08-18 21:28:40 -0300

    feat: update user tab to display user info and edit button

--- app/(tabs)/user.tsx
+++ app/(tabs)/user.tsx
@@ -1,31 +1,85 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { Link, useRouter } from 'expo-router';
 
 export default function TabTwoScreen() {
+  const router = useRouter();
+  
+  const handleEdit = () => {
+    router.push('/edit');
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      {/* <EditScreenInfo path="app/(tabs)/two.tsx" /> */}
+      <View style={styles.userWrapper}>
+        <View style={styles.userInfo}>
+          <View>
+            <Text style={styles.name}>Kayky</Text>
+            <Text style={styles.username}>@dev.kayky</Text>
+          </View>
+          <View style={styles.followInfo}>
+            <Text>Seguindo: 10</Text>
+            <Text>Seguidores: 100</Text>
+          </View>
+        </View>
+        <View style={styles.picture}>
+          
+        </View>
+      </View>
+      <View>
+        <TouchableOpacity style={styles.editBtn} onPress={handleEdit}>
+          <Text>Editar Perfil</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  editBtn: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 6,
+    marginTop: 32,
+  },
+  userWrapper: {
+    flexDirection: 'row',
+    gap: 16,
+  },
+  picture: {
+    width: 75,
+    height: 75,
+    borderRadius: 50,
+    backgroundColor: 'gray',
+    marginLeft: 'auto'
+  },
+  followInfo: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+  userInfo: {
+    gap: 8,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  username: {
+    color: 'gray',
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 16,
+    paddingTop: 16,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
   separator: {
-    marginVertical: 30,
     height: 1,
-    width: '80%',
   },
 });
