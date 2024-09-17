ed9b6f485fc1e35a9201a1484c6511346cee1923
Kayky Lopes <105399770+kaykyls@users.noreply.github.com>
2024-09-17 16:12:35 -0300

    feat: add login functionality and user authentication

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, TextInput, TouchableOpacity, Text, StyleSheet, Alert } from 'react-native';
 import { useRouter, Link, Stack } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 
@@ -10,22 +11,49 @@ const LoginScreen = () => {
     const router = useRouter();
     const colorScheme = useColorScheme();
 
-    const handleLogin = () => {
-        router.push('/');
+    const handleLogin = async () => {
+        const loginData = {
+            login: email,
+            password: password,
+        };
+
+        try {
+            const response = await fetch('https://api.papacapim.just.pro.br/sessions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(loginData),
+            });
+
+            console.log(response);
+
+            if (response.ok) {
+                const data = await response.json();
+
+                await AsyncStorage.setItem('user', JSON.stringify(data));
+
+                router.push('/');
+            } else {
+                Alert.alert('Erro', 'Login falhou. Verifique suas credenciais.');
+            }
+        } catch (error) {
+            Alert.alert('Erro', 'Algo deu errado. Tente novamente mais tarde.');
+        }
     };
 
     return (
         <>
-            <Stack.Screen options={{title: 'Login'}}/>
+            <Stack.Screen options={{ title: 'Login' }} />
             <View style={styles.container}>
-                <Text style={{color: Colors[colorScheme ?? 'light'].text}}>Email</Text>
+                <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>Email</Text>
                 <TextInput
                     style={styles.input}
                     placeholder="Email"
                     value={email}
                     onChangeText={setEmail}
                 />
-                <Text style={{color: Colors[colorScheme ?? 'light'].text}}>Senha</Text>
+                <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>Senha</Text>
                 <TextInput
                     style={styles.input}
                     placeholder="Senha"
@@ -34,14 +62,18 @@ const LoginScreen = () => {
                     onChangeText={setPassword}
                 />
                 <TouchableOpacity style={styles.button} onPress={handleLogin}>
-                    <Text style={styles.buttonText}>Login</Text>  
+                    <Text style={styles.buttonText}>Login</Text>
                 </TouchableOpacity>
                 <View style={styles.registerText}>
-                    <Text style={{color: Colors[colorScheme ?? 'light'].text, textAlign: 'center'}}>Não possui uma conta? <Link style={styles.registerLink} href="/register">Registrar-se</Link></Text>
+                    <Text style={{ color: Colors[colorScheme ?? 'light'].text, textAlign: 'center' }}>
+                        Não possui uma conta?{' '}
+                        <Link style={styles.registerLink} href="/register">
+                            Registrar-se
+                        </Link>
+                    </Text>
                 </View>
             </View>
         </>
-        
     );
 };
 
@@ -66,7 +98,7 @@ const styles = StyleSheet.create({
         marginBottom: 12,
         paddingHorizontal: 8,
         borderRadius: 6,
-        marginTop: 4
+        marginTop: 4,
     },
     button: {
         backgroundColor: '#007BFF',
@@ -74,7 +106,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         borderRadius: 5,
         width: '100%',
-        marginTop: 4
+        marginTop: 4,
     },
     buttonText: {
         color: '#FFFFFF',
