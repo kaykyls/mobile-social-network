fb03a772f2c8ee041580c02493bdb19b992fdcce
Kayky Lopes <105399770+kaykyls@users.noreply.github.com>
2024-10-21 20:36:02 -0300

    feat: create post functionality

--- app/new-post/index.tsx
+++ app/new-post/index.tsx
@@ -1,72 +1,97 @@
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, Pressable, View } from 'react-native';
+import { StyleSheet, TextInput, Pressable, View, Alert } from 'react-native';
 import { Text } from '@/components/Themed';
 import { useRouter, Stack } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const NewPost: React.FC = () => {
   const [content, setContent] = useState('');
   const router = useRouter();
 
-  const handlePost = () => {
-    console.log('Post content:', content);
-    router.push('/');
+  const handlePost = async () => {
+    const userData = await AsyncStorage.getItem('user');
+
+    if (!userData) {
+      console.error('Token not found');
+      Alert.alert('Error', 'Token not found');
+      return;
+    }
+
+    try {
+      const response = await fetch('https://api.papacapim.just.pro.br/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-session-token': JSON.parse(userData).token,
+        },
+        body: JSON.stringify({
+          post: {
+            message: content,
+          },
+        }),
+      });
+
+      if (response.ok) {
+        console.log('Post successful');
+        router.push('/');
+      } else {
+        console.error('Failed to post');
+        Alert.alert('Error', 'Failed to post');
+      }
+    } catch (error) {
+      console.error('Error posting:', error);
+      Alert.alert('Error', 'Error posting');
+    }
   };
 
   return (
     <>
-      <Stack.Screen options={{title: 'Novo post'}}/>
+      <Stack.Screen options={{ title: 'Novo post' }} />
       <View style={styles.container}>
-            <Text style={styles.title}>O que está acontecendo?</Text>
-            <TextInput
-                placeholderTextColor={'#555'}
-                style={styles.textInput}
-                multiline
-                numberOfLines={10}
-                placeholder="Escreva o seu post aqui..."
-                value={content}
-                onChangeText={setContent}
-            />
-            <Pressable style={styles.button} onPress={handlePost}>
-                <Text style={styles.buttonText}>Postar</Text>
-            </Pressable>
-        </View>
+        <Text style={styles.title}>O que está acontecendo?</Text>
+        <TextInput
+          placeholderTextColor={'#555'}
+          style={styles.textInput}
+          multiline
+          numberOfLines={10}
+          placeholder="Escreva o seu post aqui..."
+          value={content}
+          onChangeText={setContent}
+        />
+        <Pressable style={styles.button} onPress={handlePost}>
+          <Text style={styles.buttonText}>Postar</Text>
+        </Pressable>
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 20,
-        marginBottom: 16,
-        borderBottomWidth: 1,
-        borderColor: '#555',
-        paddingBottom: 16
-    },
-    container: {
-        flex: 1,
-        padding: 16,
-        justifyContent: 'space-between',
-    },
-    textInput: {
-        flex: 1,
-        borderColor: '#ccc',
-        borderRadius: 8,
-        textAlignVertical: 'top',
-        fontSize: 16,
-        marginBottom: 16
-    },
-    button: {
-        backgroundColor: '#007bff',
-        borderRadius: 8,
-        padding: 16,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  textInput: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 8,
+    marginBottom: 16,
+    borderRadius: 4,
+  },
+  button: {
+    backgroundColor: '#007bff',
+    padding: 12,
+    borderRadius: 4,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
 });
 
 export default NewPost;
